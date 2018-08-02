15e328ec2f96bbb5d824f25e01b64f110de6b543
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-08-01 22:10:50 -0500

    Add project files.

---
+++ C-Step_110/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Step_110
+{
+    public class Maths
+    {
+
+        public Maths()
+        {
+            List<int> Maths = new List<int>() { 3, 5, 10, 2 };
+            foreach (int Mat in Maths)
+            {
+                Set newAnswer = Mat * int answer;
+            }
+        }
+        public List<Maths> Mathss { get; set; }
+
+
+    }
+    
+}
---
+++ C-Step_110/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Step_110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
