868cbebdc8001fa3ec19aa9d82359f4fb0e236d2
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-08-03 20:49:39 -0500

    Complete File

--- C-Step_110/Math.cs
+++ C-Step_110/Math.cs
@@ -6,19 +6,12 @@ using System.Threading.Tasks;
 
 namespace C_Step_110
 {
-    public class Maths
+    public class Math
     {
 
-        public Maths()
-        {
-            List<int> Maths = new List<int>() { 3, 5, 10, 2 };
-            foreach (int Mat in Maths)
-            {
-                Set newAnswer = Mat * int answer;
-            }
-        }
-        public List<Maths> Mathss { get; set; }
-
+        public int AddOne { get; set; }
+        public int SubtractOne { get; set; }
+        public int Squared { get; set; }
 
     }
     
